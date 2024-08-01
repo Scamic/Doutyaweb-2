172a8f0721a81f009df8b094c0f8443b579a8c71
Wachann <wachannarkhede4@gmail.com>
2024-08-01 16:39:51 +0530

    career added

--- Doutya-WEbsite/src/App.jsx
+++ Doutya-WEbsite/src/App.jsx
@@ -5,16 +5,17 @@ import 'aos/dist/aos.css';
 import NavBar from "./components/Nav";
 import HomePage from "./components/HomePage";
 import AboutUs from "./components/about";
-import ProductSection from './components/products'; 
+import ProductSection from './components/products'; // Ensure the correct path
 import Blog from './components/Blog';
 import Footer from './components/Footer';
+import CareerSection from './components/Careers';
 
 const App = () => {
     useEffect(() => {
         AOS.init({
             duration: 1000,
             easing: 'ease-in-out',
-            once: false, 
+            once: false, // Set to false to animate every time you scroll up/down
         });
     }, []);
 
@@ -24,6 +25,7 @@ const App = () => {
             <HomePage />
             <AboutUs />
             <ProductSection />
+             <CareerSection/>
             <Blog />
             <Footer />
         </div>
